e2ef6a8f1a0ef11446ec9df9ad4b3c38156116a1
Maruf Khan <whoismaruf@outlook.com>
2021-08-14 19:43:42 +0600

    add items

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log("It's working..!");
+let tasks = [];
+
+// Get all required elements
+
+const add_task = document.getElementById('add-task');
+const task_input = document.getElementById('task-input');
+const task_list = document.getElementById('task-list');
+
+
+console.log(task_list.children)
+
+// Add click event
+
+add_task.addEventListener("click", () => {
+    const li = document.createElement('li');
+    li.innerText = task_input.value;
+    li.classList = 'item'
+    task_list.appendChild(li);
+    task_input.value = '';
+});
